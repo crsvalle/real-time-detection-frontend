151b443d471787d4267fe9e5ffee979d081112ee
crsvalle <cristian.valle@pursuit.org>
2026-03-09 10:38:13 -0400

    created basic about page

---
+++ src/app/about/page.js
@@ -0,0 +1,15 @@
+export default function AboutPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>About CarVision</h1>
+      <p>
+        CarVision is a real-time car detection app built with Next.js. 
+        Upload images of vehicles to detect cars, track their location, and 
+        explore vehicle data.
+      </p>
+      <p>
+
+      </p>
+    </div>
+  );
+}
